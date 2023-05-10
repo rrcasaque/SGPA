0ed0bbf2786e346c8f86b9cf91589d23bfeef300
Mario-Prado <mcmp.prado22@gmail.com>
2023-05-10 20:48:36 -0300

    criando checkOut

---
+++ src/main/java/com/example/sgpa/domain/usecases/checkout/CreateCheckOutUseCase.java
@@ -0,0 +1,16 @@
+package com.example.sgpa.domain.usecases.checkout;
+
+import com.example.sgpa.domain.usecases.part.PartDAO;
+
+public class CreateCheckOutUseCase {
+
+
+    CreateCheckOutUseCase(PartDAO partDAO){
+        
+    }
+
+    void createCheckout(){
+
+    }
+
+}
