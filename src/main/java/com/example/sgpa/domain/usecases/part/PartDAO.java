6a5c1c6f36a8ed2f397309d99f5139ee7b23c83e
Mario-Prado <mcmp.prado22@gmail.com>
2023-05-10 19:29:00 -0300

    Arrumando Declaração de PartDAO

--- src/main/java/com/example/sgpa/domain/usecases/part/PartDAO.java
+++ src/main/java/com/example/sgpa/domain/usecases/part/PartDAO.java
@@ -1,7 +1,8 @@
 package com.example.sgpa.domain.usecases.part;
 
+import com.example.sgpa.domain.entities.part.Part;
 import com.example.sgpa.domain.usecases.utils.DAO;
 
-public interface PartDAO extends DAO {
+public interface PartDAO extends DAO<Part, Integer> {
 
 }
