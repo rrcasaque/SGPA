f889c5c25ed4b7bcf8ce47c533d8d9de553d14f0
isaacnwt <isaacnewton02@hotmail.com>
2023-05-10 22:12:27 -0300

    feat(Part): adicionando PartDAO e ItemPartDAO

--- src/main/java/com/example/sgpa/domain/usecases/part/PartDAO.java
+++ src/main/java/com/example/sgpa/domain/usecases/part/PartDAO.java
@@ -3,6 +3,14 @@ package com.example.sgpa.domain.usecases.part;
 import com.example.sgpa.domain.entities.part.Part;
 import com.example.sgpa.domain.usecases.utils.DAO;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PartDAO extends DAO<Part, Integer> {
+    Integer create(Part obj);
+    Optional<Part> findOne(Part obj);
+    List<Part> findAll();
+    void update(Part obj);
+    boolean delete(Part obj);
 
 }
