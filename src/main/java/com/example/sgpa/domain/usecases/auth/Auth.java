2c807e60788e7968f1cc23d76f1c0deb50f1038c
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-05-14 13:57:37 -0300

    fix: remove old import

--- src/main/java/com/example/sgpa/domain/usecases/auth/Auth.java
+++ src/main/java/com/example/sgpa/domain/usecases/auth/Auth.java
@@ -1,11 +1,10 @@
 package com.example.sgpa.domain.usecases.auth;
 
-import java.util.Optional;
+import java.util.Optional; 
 
 import com.example.sgpa.domain.entities.user.Technician;
 import com.example.sgpa.domain.entities.user.User;
 import com.example.sgpa.domain.entities.user.UserType;
-import com.example.sgpa.domain.usecases.technician.TechnicianDAO;
 import com.example.sgpa.domain.usecases.user.UserDAO;
 
 public class Auth {
