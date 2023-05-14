e3812e9f690d373ba4a23fb393d1fa85836cf40b
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-05-14 19:43:15 -0300

    feat: created checkRegisteredMovements useCase

---
+++ src/main/java/com/example/sgpa/domain/usecases/utils/validation/CheckRegisteredMovementsUseCase.java
@@ -0,0 +1,26 @@
+package com.example.sgpa.domain.usecases.utils.validation;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.sgpa.domain.entities.reservation.Reservation;
+import com.example.sgpa.domain.entities.user.User;
+
+public class CheckRegisteredMovementsUseCase {
+	private ReservationDAO reservationDAO;
+
+	public CheckRegisteredMovementsUseCase(ReservationDAO reservationDAO) {		
+		this.reservationDAO = reservationDAO;
+	}
+	
+	public void check(User user){		
+		List<Reservation> reservationList = reservationDAO.getAllReservations();
+		boolean state = false;
+		for(Reservation reservation : reservationList){
+			if(reservation.getRequester().equals(user))
+				state = true;
+		}
+		if(!state)
+			throw new RuntimeException("moves not found!");
+	}
+}
