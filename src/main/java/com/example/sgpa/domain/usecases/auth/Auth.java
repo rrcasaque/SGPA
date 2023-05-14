d7b3835c5af8220127bdf1be0b9fdf1b308166c3
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-05-14 13:56:38 -0300

    fix: removed unnecessary interface

--- src/main/java/com/example/sgpa/domain/usecases/auth/Auth.java
+++ src/main/java/com/example/sgpa/domain/usecases/auth/Auth.java
@@ -3,20 +3,24 @@ package com.example.sgpa.domain.usecases.auth;
 import java.util.Optional;
 
 import com.example.sgpa.domain.entities.user.Technician;
+import com.example.sgpa.domain.entities.user.User;
+import com.example.sgpa.domain.entities.user.UserType;
 import com.example.sgpa.domain.usecases.technician.TechnicianDAO;
+import com.example.sgpa.domain.usecases.user.UserDAO;
 
 public class Auth {
-	private TechnicianDAO technicianDAO;
+	private UserDAO userDAO;
 
-	public Auth(TechnicianDAO technicianDAO) {		
-		this.technicianDAO = technicianDAO;
+	public Auth(UserDAO userDAO) {		
+		this.userDAO = userDAO;
 	}
 	
 	public boolean authenticate(String institutionalId, String password){
-		Optional<Technician> technician = technicianDAO.findOne(institutionalId);		
-		if(technician.isEmpty())
-			throw new RuntimeException("Technician user not found");	
-		if(!technician.get().getPassword().equals(password))
+		Optional<User> user = userDAO.findOneByIdAndType(UserType.TECHNICIAN,institutionalId);
+		Technician technician = (Technician) user.get(); 
+		if(technician == null)
+			throw new RuntimeException("Technician user not found");		
+		if(!technician.getPassword().equals(password))
 			throw new RuntimeException("Incorrect password");	
 		return true;		
 	}
--- src/main/java/com/example/sgpa/domain/usecases/technician/TechnicianDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.sgpa.domain.usecases.technician;
-
-import com.example.sgpa.domain.entities.user.Technician;
-import com.example.sgpa.domain.usecases.utils.DAO;
-
-public interface TechnicianDAO extends DAO<Technician, String>{
-	
-}
