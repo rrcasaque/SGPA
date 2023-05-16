9525e247063cb5c57519f90926d28be415f0f50b
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-05-16 11:33:12 -0300

    feat: created checkRegisteredReservation useCase

---
+++ src/main/java/com/example/sgpa/domain/usecases/utils/validation/CheckRegisteredReservationUseCase.java
@@ -0,0 +1,21 @@
+package com.example.sgpa.domain.usecases.utils.validation;
+
+import java.util.Date;
+import java.util.List;
+
+import com.example.sgpa.domain.entities.reservation.Reservation;
+import com.example.sgpa.domain.usecases.reservation.ReservationDAO;
+
+public class CheckRegisteredReservationUseCase {
+	private ReservationDAO reservationDAO;
+
+	public CheckRegisteredReservationUseCase(ReservationDAO reservationDAO) {		
+		this.reservationDAO = reservationDAO;
+	}
+	
+	public void check(Date start, Date end){
+		List<Reservation> reservationList = reservationDAO.findByDate(start,end);
+		if(reservationList.isEmpty())
+			throw new RuntimeException("moves not found!");
+	}
+}
