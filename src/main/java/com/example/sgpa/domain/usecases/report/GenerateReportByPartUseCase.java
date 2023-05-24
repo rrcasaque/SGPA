237bfdfb9523763b86a906cf769abbd0d684c672
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-05-23 23:41:23 -0300

    refactor: refactor useCases to validate in your own scopes

--- src/main/java/com/example/sgpa/domain/usecases/report/GenerateReportByPartUseCase.java
+++ src/main/java/com/example/sgpa/domain/usecases/report/GenerateReportByPartUseCase.java
@@ -3,21 +3,40 @@ package com.example.sgpa.domain.usecases.report;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.example.sgpa.domain.entities.historical.Event;
 import com.example.sgpa.domain.entities.part.Part;
 import com.example.sgpa.domain.entities.reservation.Reservation;
+import com.example.sgpa.domain.usecases.historical.EventDAO;
+import com.example.sgpa.domain.usecases.part.PartDAO;
 import com.example.sgpa.domain.usecases.reservation.ReservationDAO;
+import com.example.sgpa.domain.usecases.utils.validation.CheckExistencePartUseCase;
+import com.example.sgpa.domain.usecases.utils.validation.CheckExistenceUserUseCase;
+import com.example.sgpa.domain.usecases.utils.validation.DateType;
+import com.example.sgpa.domain.usecases.utils.validation.VerifyDateUseCase;
 
 public class GenerateReportByPartUseCase {
-	private ReservationDAO reservationDAO;
+	private EventDAO eventDAO;
+	private PartDAO partDAO;
 
-	public GenerateReportByPartUseCase(ReservationDAO reservationDAO) {		
-		this.reservationDAO = reservationDAO;
+	public GenerateReportByPartUseCase(EventDAO eventDAO, PartDAO partDAO) {		
+		this.eventDAO = eventDAO;
+		this.partDAO = partDAO;
 	}
 	
-	public List<Reservation> generate(Part part, LocalDateTime start, LocalDateTime end) {		
-		List<Reservation> reservationList = reservationDAO.getReportByPart(part, start, end); 
-		if(reservationList.isEmpty())
+	public List<Event> generate(Part part, LocalDateTime start, LocalDateTime end) {
+		try {
+			CheckExistencePartUseCase checkExistenceUserUseCase = new CheckExistencePartUseCase(this.partDAO);
+			checkExistenceUserUseCase.check(part);
+			VerifyDateUseCase.verify(DateType.START, start);
+			VerifyDateUseCase.verify(DateType.END, end);
+			
+			List<Event> eventList = eventDAO.getReportByPart(part, start, end); 
+			
+			if(eventList.isEmpty())
+				throw new RuntimeException("data not found for the informed parameters");
+			return eventList;
+		} catch (Exception e) {
 			throw new RuntimeException("data not found for the informed parameters");
-		return reservationList;
+		}		
 	}
 }
