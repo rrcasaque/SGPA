437b42964ff5b4f911d980e62ca77360a1b1b913
Mario-Prado <mcmp.prado22@gmail.com>
2023-05-27 12:05:31 -0300

    carregando sgpa

--- src/main/java/com/example/sgpa/application/view/HelloApplication.java
+++ src/main/java/com/example/sgpa/application/view/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginUi.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("SGPA");
         stage.setScene(scene);
         stage.show();
     }
